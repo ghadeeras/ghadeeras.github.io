bc56ad93157eb32c26d78d54c064fe63a3f77c4d
Ghadeer <ghadeeras@users.noreply.github.com>
2026-08-25 22:23:26 -0400

    Removing the WebGL Lab toy :( ... and some dead code

--- js/utils/legacy/gear/index.js
+++
@@ -1,6 +0,0 @@
-export * from './dragging.js';
-export * from './effects.js';
-export * from './ui.input.js';
-export * from './ui.output.js';
-export * from './value.js';
-//# sourceMappingURL=index.js.map
--- src/utils/legacy/gear/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './dragging.js'
-export * from './effects.js'
-export * from './ui.input.js'
-export * from './ui.output.js'
-export * from './value.js'
